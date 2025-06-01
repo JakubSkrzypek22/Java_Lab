f790ec81dcbeec742d023ddfc426f6b486448844
JakubSkrzypek22 <jakubskrzypek005@gmail.com>
2025-06-01 19:49:29 +0200

    Add files via upload

---
+++ Laboratorium1/src/Zad/Zadanie1.java
@@ -0,0 +1,11 @@
+package Zad;
+
+public class Zadanie1 {
+
+    String imie = "Jakub";
+    int wiek = 23;
+
+    public void zad1(){
+        System.out.println("Moje imie to " + imie + " i mam " + wiek  + " lat");
+    }
+}
