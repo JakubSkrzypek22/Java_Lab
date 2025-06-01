956ee7f8efcdf82a6403ef70670cbf3bfcf00f01
JakubSkrzypek22 <jakubskrzypek005@gmail.com>
2025-06-01 19:27:47 +0200

    Add files via upload

---
+++ Laboratorium3/src/Zadania/Zadanie3.java
@@ -0,0 +1,13 @@
+package Zadania;
+
+public class Zadanie3 {
+
+    public  void zad3(){
+
+        String [] tab = {"pies" , "małpa" , "ptak" , "jeż"};
+
+        for (String wyraz : tab){
+            System.out.println(wyraz.toUpperCase());
+        }
+    }
+}
